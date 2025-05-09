764b7308a3b915aead935dfcb06d28033a75e011
javiersg <jsdash10000@gmail.com>
2025-05-09 10:26:39 -0700

    Se creo el portafolio

--- portafolio/src/App.tsx
+++ portafolio/src/App.tsx
@@ -1,22 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Home from './views/home/home';
-import About from './views/about/about';
-import Header from './components/header';
-import Contact from './views/contact/contact';
-import Projects from './views/projects/projects';
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
